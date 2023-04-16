48e68622528dc7d0d09dd2aee07170aaecdd5336
Martín <magadiflo@gmail.com>
2023-04-16 00:06:41 -0500

    Modificando código de shared-library y verificando que al ejecutar los proyectos que usan esa dependencia muestren el cambio al ser ejecutados

--- shared-library/src/main/java/com/magadiflo/my/company/app/CommonUtility.java
+++ shared-library/src/main/java/com/magadiflo/my/company/app/CommonUtility.java
@@ -2,6 +2,6 @@ package com.magadiflo.my.company.app;
 
 public class CommonUtility {
     public static String getAppName() {
-        return "My Company Project Beta Version";
+        return "My Company Project Ultimate Version -> Project Finalized";
     }
 }
